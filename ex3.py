c75b8c40ae0cd01065205556036405f2368791ca
oronBortman <oron.bortman@gmail.com>
2021-02-03 11:30:39 +0200

    Add documentation and examples of transactions

--- ex3.py
+++ ex3.py
@@ -1,8 +1,12 @@
 from collections import defaultdict, deque
 
+""" -------------------------------------------------------------------
+-------------------------- Class Decelerations ------------------------
+------------------------------------------------------------------- """
 
-# Class to represent an element
-class Element:
+
+# Class to represent a transaction
+class Transaction:
     def __init__(self, element_str):
         self.trans_num = element_str[1]
         self.trans_act = element_str[0]
@@ -25,15 +29,16 @@ class Graph:
         self.V.add(v)
 
 
-'''
-Algorithm:
-1. Split the string into elemnts with ';' as a seperator and add it to list
-2. run over the list and for every elemnt:
-    2.1 if it is read, check if there is element in list after him with write, different transaction number and same Table
-    2.2 if it is write, check if there is clement in list after him with write/read, different transaction number and same Table
-    if 2.1 or 2.2 applies then add the source transaction and dest transcation as vertices if they do not exist.
-    After that, add the dest transaction vert in the list of the source trans vert.
-'''
+""" -------------------------------------------------------------------
+------------------------------ Graph Functions ------------------------
+------------------------------------------------------------------- """
+
+
+def build_precedence_graph(elem_list, g):
+    index = 0
+    for e in elem_list:
+        check_for_vert_and_add_to_graph(e, elem_list, index + 1, g)
+        index = index + 1
 
 
 def check_for_vert_and_add_to_graph(source_elem, tran_list, index_to_look_from, g):
@@ -41,62 +46,113 @@ def check_for_vert_and_add_to_graph(source_elem, tran_list, index_to_look_from, 
     for dest_elem in tran_list:
         if source_elem.trans_act == 'R' and dest_elem.trans_act == 'W' or dest_elem.trans_act == 'W':
             if source_elem.trans_scheme == dest_elem.trans_scheme and source_elem.trans_num != dest_elem.trans_num:
-                g.add_edge(source_elem.trans_num, dest_elem.trans_num) #Adding edge
-                g.add_vert(source_elem.trans_num) # add source transaction as vert
-                g.add_vert(dest_elem.trans_num) # add destination transaction as vert
+                g.add_edge(source_elem.trans_num, dest_elem.trans_num)  # Adding edge
+                g.add_vert(source_elem.trans_num)  # add source transaction as vert
+                g.add_vert(dest_elem.trans_num)  # add destination transaction as vert
 
 
-def build_graph(elem_list, g):
-    index = 0
-    for e in elem_list:
-        check_for_vert_and_add_to_graph(e, elem_list, index + 1, g)
-        index = index + 1
+""" -------------------------------------------------------------------
+---------------------- Topological Order Functions --------------------
+------------------------------------------------------------------- """
 
 
-def topological_sort(g):
-    queue = deque([])
-    in_degree = {}
+def print_trans_by_queue_state(u, queue):
+    if len(queue) == 0:
+        print("T{0}".format(u), end='')
+    else:
+        print("T{0}->".format(u), end='')
 
-    for v in g.V:
-        in_degree[v] = 0
 
-    for s_vert, d_vert_list in g.graph.items():
-        for d_vert in d_vert_list:
-            in_degree[d_vert] = in_degree[d_vert] + 1
-
-    for v in g.V:
-        if in_degree[v] == 0:
-            queue.append(v)
-
-    while len(queue) != 0:
-        u = queue.pop()
-
-        for v in g.graph[u]:
-            in_degree[v] = in_degree[v] - 1
-            if in_degree[v] == 0:
-                queue.append(v)
-
-        if len(queue) == 0:
-            print("T{0}".format(u), end='')
-        else:
-            print("T{0}->".format(u), end='')
-
+def check_if_there_is_cycle(g, in_degree):
     for v in g.V:
         if in_degree[v] != 0:
             print("No")
             return
 
 
+def init_in_degree_by_vertices(g, in_degree):
+    for v in g.V:
+        in_degree[v] = 0
+
+
+def update_in_degree_by_edges(g, in_degree):
+    for s_vert, d_vert_list in g.graph.items():
+        for d_vert in d_vert_list:
+            in_degree[d_vert] = in_degree[d_vert] + 1
+
+
+def add_to_queue_vertices_with_zero_in_degree(g, in_degree, queue):
+    for v in g.V:
+        add_to_queue_vert_if_with_zero_in_degree(v, in_degree, queue)
+
+
+def add_to_queue_vert_if_with_zero_in_degree(v, in_degree, queue):
+    if in_degree[v] == 0:
+        queue.append(v)
+
+
+def main_loop_of_topological_sort(g, in_degree, queue):
+    while len(queue) != 0:
+        u = queue.pop()
+        for v in g.graph[u]:
+            in_degree[v] = in_degree[v] - 1
+            add_to_queue_vert_if_with_zero_in_degree(v, in_degree, queue)
+        print_trans_by_queue_state(u, queue)
+
+
+def topological_sort(g):
+    queue = deque([])
+    in_degree = {}
+
+    # INIT
+    init_in_degree_by_vertices(g, in_degree)
+    update_in_degree_by_edges(g, in_degree)
+    add_to_queue_vertices_with_zero_in_degree(g, in_degree, queue)
+
+    # Main loop
+    main_loop_of_topological_sort(g, in_degree, queue)
+
+    # Check all nodes reached
+    check_if_there_is_cycle(g, in_degree)
+
+
+""" -------------------------------------------------------------------
+--------------------------------- MAIN --------------------------------
+------------------------------------------------------------------- """
+
+'''
+Algorithm:
+
+1. Create a list of transactions elements by splitting the transactions string 
+   into transaction elements with ';' as a separator.
+
+2. Build precedence graph:
+
+    2.1 For every transaction in the list:
+
+        2.1.1 if it's action is read, check if there is transaction in list after him with the action write, 
+              different transaction number and same Table.
+    
+        2.1.2 If it's action is write, check if there is transaction in list after him with action write/read,
+              different transaction number and same Table
+    
+        2.1.3 If 2.1.1 or 2.1.2 applies:
+              2.1.3.1 If the source and dest transaction vertices doesnt exist in the precedence graph,
+                      create them.
+              2.1.3.2 Add the edge (source transaction vert, dest transaction vert) to the precedence graph.
+    
+3. Run topological sort algorithm and print 'No' if there is cycle.
+   If there is no cycle, print the topological order.
+'''
+
+# Get input of transactions and processes it
+transactions_str = input("Enter transactions: ")
+transaction_str_list = transactions_str.split(";")
+transaction_list = []
+for t in transaction_str_list:
+    transaction_list.append(Transaction(t))
+
+# Runs the algorithm
 graph = Graph()
-#transaction = "R2(A);R1(B);W2(A);R2(B);R3(A);W1(B);W3(A);W2(B)" # Cycle
-#transaction = "R2ׂׂ(A);R1(B);W2(A);R3(A);W1(B);W3(A);R2(B);W2(B)" # T1->T2->T3
-transaction = "R5(A);W5(C);W5(B);R5(B);W4(C);R4(A);R4(B);W4(A);W3(A);W3(B);W3(C);R3(C);R2(A);W2(A);W2(B);R2(B);R1(A);W1(A);R1(A)" #T5->T4->T3->T2->T1
-transaction = "R1(A);W3(B);R1(A);W1(A);W3(A);W3(C);R3(C);W5(C);R5(A);R4(B);W2(B);W4(C);R4(A);R2(A);R2(B);R4(C)" #T1->T3->T5->T4->T2
-transaction = "R5(A);W4(C);W3(A);R2(A);R1(A);W5(C);R4(A);W3(B);W2(A);W1(A);W5(B);R5(B);R4(B);W4(A);W3(C);R3(C);R1(A)" #Cycle
-transaction = "W4(C);R4(A);R2(A);R1(A);W1(A);W2(A);W2(B);R2(B);R1(A);W3(A);W3(B);W3(C);R3(C);R4(B);W4(A);R5(A);W5(C);W5(B);R5(B)" #Cycle
-transaction_list = transaction.split(";")
-element_list = []
-for t in transaction_list:
-    element_list.append(Element(t))
-build_graph(element_list, graph)
+build_precedence_graph(transaction_list, graph)
 topological_sort(graph)
